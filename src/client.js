ce019e918754a5857afd7e921ea3d8947404e6c3
Charlie Birks <charlie@daftgames.net>
2019-03-17 20:52:23 +0000

    Add a little dark mode
    Surprisingly easy

--- src/client.js
+++ src/client.js
@@ -3,3 +3,7 @@ import * as sapper from '../__sapper__/client.js';
 sapper.start({
 	target: document.querySelector('#sapper')
 });
+
+// a little hacky
+if(new URLSearchParams(location.search).has("dark"))
+	document.body.classList.add("dark-mode");
